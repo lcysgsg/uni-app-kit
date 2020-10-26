b5b5a0c7343164a14c66ee9b9dd40576a3af0c43
lcysgsg <249313573@qq.com>
2020-10-26 10:56:31 +0800

    fix：purge 范围错误导致的编译过久的bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./**/*.html', './**/*.vue', './**/*.js'],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.js'],
   theme: {
     extend: {},
   },
