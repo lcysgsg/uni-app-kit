5b1d1e6ef24c96486d7a74cccb34ec2cb1f6d4e6
LiChaoYang <249313573@qq.com>
2021-09-13 16:16:07 +0800

    init(开发环境 eslint, prettier, stylelint, ts)

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+'use strict';
+/** @format */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
